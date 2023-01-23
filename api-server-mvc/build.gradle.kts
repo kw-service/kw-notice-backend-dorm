b7fa07e25325bd844d4836ab3edd642ac9074d3f
tianea2160 <rhlehfndvkd7557@daum.net>
2023-01-23 16:06:10 +0900

    feat : feat swagger 3.0

--- api-server-mvc/build.gradle.kts
+++ api-server-mvc/build.gradle.kts
@@ -2,6 +2,9 @@ dependencies{
     implementation(project(":dormitory-domain"))
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.1")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.1")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
 }
 
 tasks.jar{
