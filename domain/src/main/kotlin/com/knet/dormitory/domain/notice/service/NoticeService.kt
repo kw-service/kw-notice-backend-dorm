8fdd8510bda98316b90259b8fb51550b8e9a75e1
tianea2160 <rhlehfndvkd7557@daum.net>
2023-01-31 11:25:29 +0900

    fix : add id, url field in /notice endpoint

--- domain/src/main/kotlin/com/knet/dormitory/domain/notice/service/NoticeService.kt
+++ domain/src/main/kotlin/com/knet/dormitory/domain/notice/service/NoticeService.kt
@@ -1,10 +1,11 @@
 package com.knet.dormitory.domain.notice.service
 
 import com.knet.dormitory.domain.notice.Notice
+import com.knet.dormitory.domain.notice.dto.NoticeDetailDTO
 import com.knet.dormitory.domain.notice.dto.NoticeShortDTO
 
 interface NoticeService {
     fun getNoticeList(page:Int, size:Int) : List<NoticeShortDTO>?
     fun getNoticeTotalCount() : Int?
-    fun findAll() : List<Notice>
+    fun findAll() : List<NoticeDetailDTO>
 }
