2dd556e17a9fa7bd3f6ff4fde08d75b0492433d4
tianea2160 <rhlehfndvkd7557@daum.net>
2023-02-03 00:25:54 +0900

    feat, fix : create dockerfile for containerization and fix init.sql

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-create table notice
-(
-    notice_id   varchar(36) primary key,
-    title       varchar(255) null,
-    writer_name varchar(255) null,
-    writer_id   varchar(255) null,
-    created_at  date         null,
-    topic       varchar(255) not null
-);
