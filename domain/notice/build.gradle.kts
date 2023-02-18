4ae4892b9425be77e7b93db2ea719b4db0d91208
tianea2160 <rhlehfndvkd7557@daum.net>
2023-02-18 15:09:59 +0900

    refactor : split domain module and web module

---
+++ domain/notice/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins{
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.7.22"
+}
+
+dependencies{
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
+
+repositories {
+    mavenCentral()
+}
+
+// class open
+allOpen{
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+// class no arg constructor
+noArg{
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+tasks.bootJar{
+    enabled = false
+}
+
+tasks.jar{
+    enabled = true
+}
---
+++ domain/notice/src/main/kotlin/com/knet/dormitory/config/JpaConfig.kt
@@ -0,0 +1,9 @@
+package com.knet.dormitory.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+}
