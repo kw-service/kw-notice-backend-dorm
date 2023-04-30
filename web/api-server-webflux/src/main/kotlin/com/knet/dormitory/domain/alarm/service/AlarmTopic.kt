24cdf8ec8f6b00f1ae26147f97dc6d28206d341d
tianea2160 <rhlehfndvkd7557@daum.net>
2023-04-30 11:26:07 +0900

    fix : remove webflux module that no more use

--- web/api-server-webflux/src/main/kotlin/com/knet/dormitory/domain/alarm/service/AlarmTopic.kt
+++
@@ -1,5 +0,0 @@
-package com.knet.dormitory.domain.alarm.service
-
-enum class AlarmTopic {
-    COMMON, REFRIGERATOR, REGULAR_RECRUITMENT
-}
--- web/api-server-webflux/src/main/kotlin/com/knet/dormitory/shard/dto/CommonResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.knet.dormitory.shard.dto
-
-import org.springframework.http.HttpStatus
-
-class CommonResponse(val status: HttpStatus, val message: String)
